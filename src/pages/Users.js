01ac052d786f06535cd82892a50630cf80e7a26e
Catalin Chiorean <catalinchiorean@3pillarglobal.com>
2021-04-22 11:18:15 +0300

    Add component which gets the users from the fake server

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -2,16 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+import UsersList from '../server/UsersList';
 
 export default function Users() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-  }, [])
-
   return (
     <Container>
       <CssBaseline />
@@ -21,12 +14,10 @@ export default function Users() {
         </Typography>
       </div>
       <div>
-        {users.map(user => (
-          <Typography variant="h6" key={user.id}>
-            {user.email}
-          </Typography>
-        ))}
-      </div>
+        <Typography variant="h6">
+          <UsersList />
+        </Typography>
+      </div> 
     </Container>
 
   )
---
+++ src/server/UsersList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class UsersList extends React.Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/users')
+      .then(res => {
+        this.setState({ users: res.data});
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.users.map(user => <li>{user.email}</li>)}
+      </ul>
+    )
+  }
+}
